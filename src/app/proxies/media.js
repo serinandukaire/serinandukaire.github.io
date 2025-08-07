c75fa883255680113b5300b416862c437acb3315
Serina Ndukaire <serinadera@gmail.com>
2025-08-07 14:19:40 +0000

    email

--- src/app/proxies/media.js
+++ src/app/proxies/media.js
@@ -8,6 +8,8 @@ export default {
         if (name === "discord") return `https://${websites.discord}${target.discord.id}`
         if (name === "discordTag") return target.discord.tag
 
-        return `${name === "email" ? "" : "https://"}${websites[name] ?? ""}${target[name]}`
+       return name === "email"
+        ? `mailto:${target[name]}`
+        : `https://${websites[name] ?? ""}${target[name]}`
     }
 }
