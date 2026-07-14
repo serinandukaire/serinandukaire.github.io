4de2d50da4eee892b6cffdcf1a7da8b6c3cbb3a2
serinandukaire <serinadera@gmail.com>
2026-07-14 15:43:02 +0000

    Deploying to gh-pages from @ serinandukaire/serinandukaire.github.io@1cebb281d214b66a418b124ab9d15ad1d4af2db1 🚀

--- js/spaHandler.js
+++ js/spaHandler.js
@@ -9,15 +9,15 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/app/spaHandler.js":
+/***/ "./src/app/spaHandler.js"
 /*!*******************************!*\
   !*** ./src/app/spaHandler.js ***!
   \*******************************/
-/***/ (() => {
+() {
 
 eval("{\n// From https://github.com/rafgraph/spa-github-pages\n\nlet pathSegmentsToKeep = 0;\nlet l = window.location;\n\nl.replace(\n    l.protocol +\n        \"//\" +\n        l.hostname +\n        (l.port ? \":\" + l.port : \"\") +\n        l.pathname\n            .split(\"/\")\n            .slice(0, 1 + pathSegmentsToKeep)\n            .join(\"/\") +\n        \"/?/\" +\n        l.pathname\n            .slice(1)\n            .split(\"/\")\n            .slice(pathSegmentsToKeep)\n            .join(\"/\")\n            .replace(/&/g, \"~and~\") +\n        (l.search\n            ? \"&\" + l.search.slice(1).replace(/&/g, \"~and~\")\n            : \"\") +\n        l.hash\n);\n\n//# sourceURL=webpack://portfolio/./src/app/spaHandler.js?\n}");
 
-/***/ })
+/***/ }
 
 /******/ 	});
 /************************************************************************/
@@ -25,7 +25,7 @@ eval("{\n// From https://github.com/rafgraph/spa-github-pages\n\nlet pathSegment
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
+/******/ 	let __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/app/spaHandler.js"]();
 /******/ 	
 /******/ })()
